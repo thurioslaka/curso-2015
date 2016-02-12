4eb67956015bdae07d808caf653c4e3fd8e94c98
thurioslaka <ram-cas@hotmail.com>
2016-02-12 19:55:14 +0100

    Proyecto Hello world
    aprendiendo comunicacion con el arduino

---
+++ Serial-2015/Proyecto Hello world/Programa-1/Programa-1.ino
@@ -0,0 +1,8 @@
+void setup ()
+ {// como el setup solo corre una vez el mensaje saldra una vez solo
+Serial.begin(9600); // la velocidad con la que se counica el arduino con el ordenador
+Serial.print("Hellow world!");
+}// El arduino hablando conmigo
+void loop () 
+{
+} // lo pongo por que tiene que llevarlo en este programa no hace nada
