2f3c89c980c10a7fd95e8884fbd8a187321437ce
thurioslaka <ram-cas@hotmail.com>
2016-02-15 16:19:13 +0100

    Programa-4
    Fet

---
+++ Serial-2015/Proyecto Hello world/Programa-4/Programa-4.ino
@@ -0,0 +1,12 @@
+void setup ()
+{
+Serial.begin (9600);
+delay(1000);
+Serial.println("Escull el numero d'operacio que vols realitzar?");
+Serial.println("1. Comprovar numero de tarjeta de credit");
+Serial.println("2. Comprovar numero de compte bancari");
+Serial.println("3. Buscar un digit perdut de tarjeta de credit");
+}
+void loop()
+{
+}
